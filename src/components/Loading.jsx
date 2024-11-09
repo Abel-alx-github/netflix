49bdd0555f0294e8a75fdba6c25f52bfd1535c38
Abel Tibeso <abeltibeso323@gmail.com>
2024-11-09 14:04:43 +0300

    Loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Loading = () => {
+    
+  return (
+    <div
+    className='absolute top-0 bottom-0 left-0 right-0 z-[100000000] bg-black/75 flex items-center justify-center text-2xl'
+    >Loading....</div>
+  )
+}
+
+export default Loading
